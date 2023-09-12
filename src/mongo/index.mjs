929806d95afc1338440c9dd210e5135f4a36efae
Christian <christianbur1248@gmail.com>
2023-09-12 17:23:14 -0500

    mongodb

--- src/mongo/index.mjs
+++ src/mongo/index.mjs
@@ -2,6 +2,6 @@ import mongoose from "mongoose";
 
 
 export const startConnection = async () => {
-  const url = encodeURI("mongodb+srv://aaaaa:077olcr1zBb1n7fC@cluster0.doacvbo.mongodb.net/?retryWrites=true&w=majority");
+  const url = encodeURI("mongodb+srv://root2:sapa@cluster0.kevtnzo.mongodb.net/?retryWrites=true&w=majority");
   await mongoose.connect(url);
 }
