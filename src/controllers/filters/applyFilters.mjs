0889377a4f830a100b1faaeaa1c9b45aff328a67
Christian <christianbur1248@gmail.com>
2023-09-16 17:46:01 -0500

    DOCKER

--- src/controllers/filters/applyFilters.mjs
+++ src/controllers/filters/applyFilters.mjs
@@ -1,23 +1,23 @@
 import Process from "../../models/Process.mjs"
 import Joi from "joi";
-import Boom  from "@hapi/boom";
+import Boom from "@hapi/boom";
 import { BLUR_FILTER, GREYSCALE_FILTER, NEGATIVE_FILTER } from "../../commons/constans.mjs";
 
 const PayloadValidation = Joi.object({
     filters: Joi.array().min(1).items(Joi.string().valid(
-        NEGATIVE_FILTER,GREYSCALE_FILTER,BLUR_FILTER
+        NEGATIVE_FILTER, GREYSCALE_FILTER, BLUR_FILTER
     ))
 })
 const applyFilters = async (payload) => {
     try {
         await PayloadValidation.validateAsync(payload)
-    }catch(error){
-        throw Boom.badData(error.message, {error})
+    } catch (error) {
+        throw Boom.badData(error.message, { error })
     }
-    
+
     const newProcess = new Process();
     newProcess.filters = payload.filters;
-    await  newProcess.save()
+    await newProcess.save()
     return newProcess
 }
 
