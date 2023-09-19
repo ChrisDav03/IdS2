19dca3353f522beaa66d8302ab1b235dddda790e
Christian <christianbur1248@gmail.com>
2023-09-19 16:20:36 -0500

    Multer

--- index.mjs
+++ index.mjs
@@ -1,39 +1,46 @@
-import Express  from "express";
+import Express from "express";
 import bodyParser from "body-parser";
 import { startConnection } from "./src/mongo/index.mjs";
 import FiltersRouter from "./src/handlers/filters/index.mjs";
-import  Boom  from "@hapi/boom";
+import Boom from "@hapi/boom";
 import { PORT } from "./src/commons/env.mjs";
+import multer from "multer"; 
 
 const app = Express();
 app.use(bodyParser.json());
 
-app.get("/", (req, res) =>{
-  res.send("OK")
-})
+
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+
+app.get("/", (req, res) => {
+  res.send("OK");
+});
+
+
+app.post("/images", upload.single("file"), (req, res) => {
+ 
+  res.send("Archivo cargado correctamente");
+});
 
 app.use("/images", FiltersRouter);
 
-app.use((error, req,res,next) => {
-  if(error){
-    let err = Boom.isBoom(error) ? error: Boom.internal(error)
+app.use((error, req, res, next) => {
+  if (error) {
+    let err = Boom.isBoom(error) ? error : Boom.internal(error);
     const statusCode = err.output.statusCode;
-    const payload = err.output.payload
-    return res.status(statusCode).json(payload)
+    const payload = err.output.payload;
+    return res.status(statusCode).json(payload);
   }
 
   return next;
-})
+});
 
 const startServer = async () => {
   await startConnection();
-  app.listen(PORT, ()=> {
-    console.log(`http://localhost:${PORT}`)
-  })
-
   app.listen(PORT, () => {
-    console.log("http://localhost:3000")
-  })
-}
+    console.log(`http://localhost:${PORT}`);
+  });
+};
 
 startServer();
