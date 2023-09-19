c6c54129ef979359515c41f22a59cfcff8a169ca
Christian <christianbur1248@gmail.com>
2023-09-19 18:09:12 -0500

    act 0.2

--- index.mjs
+++ index.mjs
@@ -4,11 +4,10 @@ import { startConnection } from "./src/mongo/index.mjs";
 import FiltersRouter from "./src/handlers/filters/index.mjs";
 import Boom from "@hapi/boom";
 import { PORT } from "./src/commons/env.mjs";
-import multer from "multer"; 
-import fs from "fs";
+import multer from "multer";
+
 const app = Express();
 app.use(bodyParser.json());
-
 
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
@@ -17,27 +16,17 @@ app.get("/", (req, res) => {
   res.send("OK");
 });
 
-
-app.post("/images", upload.single("file"), (req, res) => {
-  if (!req.file) {
+app.post("/images", upload.array("images"), (req, res) => {
+  if (!req.files || req.files.length === 0) {
     return res.status(400).send("No se proporcionó un archivo");
   }
   if (!req.body.filters) {
     return res.status(400).send("No se proporcionó un filtro");
   }
-  
-  const nombreArchivo = "nombre_personalizado.jpg"; 
 
+  console.log(req.body);
 
-  fs.writeFile(nombreArchivo, req.file.buffer, (err) => {
-    if (err) {
-      console.error("Error al guardar el archivo:", err);
-      return res.status(500).send("Error al guardar el archivo");
-    }
-
-   
-    res.send("Archivo guardado correctamente");
-  });
+  res.send("Archivo enviado correctamente");
 });
 
 app.use("/images", FiltersRouter);
